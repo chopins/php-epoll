187d6f2f8eec0b56ab5bfd1730c0896a855784fc
chopins <chopins.xiao@gmail.com>
2019-09-22 21:56:19 +0800

    update

--- src/EpollEvent.php
+++ src/EpollEvent.php
@@ -1,12 +1,12 @@
 <?php
 
-class EpollEvent
+class EpollEvent implements \ArrayAccess
 {
     /**
      * @var FFI\CData
      */
     private $events = null;
-
+    private $num = 1;
     /**
      * init epoll_event struct
      * 
@@ -15,6 +15,10 @@ class EpollEvent
      */
     public function __construct(Epoll $epoll, int $num = 1)
     {
+        if ($num < 1) {
+            throw new InvalidArgumentException('EpollEvent::__construct() of paramter 2 must be greater than 0');
+        }
+        $this->num = $num;
         if ($num > 1) {
             $this->events = $epoll->ffi()->new("epoll_event[$num]");
         } else {
@@ -26,25 +30,42 @@ class EpollEvent
      * set Epoll events
      * 
      * @param int $event
+     * @param int $idx
      */
-    public function setEvent(int $event)
+    public function setEvent(int $event, int $idx = 0)
     {
-        $this->events->events = $event;
+        if ($this->num > 1) {
+            if ($idx < 1) {
+                throw new InvalidArgumentException('EpollEvent::setEvent() of paramter 2 muset be greater than 0');
+            }
+            $this->events[$idx]->events = $event;
+        } else {
+            $this->events->events = $event;
+        }
     }
 
     /**
      * set user data variable
      * 
      * @param array $data
+     * @param int $idx
      */
-    public function setData(array $data)
+    public function setData(array $data, $idx = 1)
     {
         $keys = ['ptr', 'fd', 'u32', 'u64'];
+        if ($this->num > 1) {
+            if ($idx < 1) {
+                throw new InvalidArgumentException('EpollEvent::setData() of paramter 2 muset be greater than 0');
+            }
+            $ev = $this->events[$idx];
+        } else {
+            $ev = $this->events;
+        }
         foreach ($data as $k => $v) {
             if (!in_array($k, $keys)) {
                 throw new TypeError("EpollEvent::setData(): key $k is not allow");
             }
-            $this->events->data->$k = $v;
+            $ev->data->$k = $v;
         }
     }
 
@@ -58,15 +79,16 @@ class EpollEvent
         return $this->events;
     }
 
-    public function __get($name)
+    public function offsetExists($offset): bool
     {
-        return $this->events->$name;
+        return isset($this->events[$offset]);
     }
-    public function __set($name, $value)
+    public function offsetGet($offset)
     {
-        if ($name === 'data') {
-            $this->setData($value);
-        }
-        $this->events->$name = $value;
+        return $this->events[$offset];
     }
+    public function offsetSet($offset, $value)
+    { }
+    public function offsetUnset($offset)
+    { }
 }
